c63f97f953ea1537555343d93dff16dc1b2aff21
nurqk.x <145999046+PopularLane@users.noreply.github.com>
2026-07-21 21:35:37 +0500

    Add: first-unique-number - submission-0

---
+++ Data Structures & Algorithms/first-unique-number/submission-0.py
@@ -0,0 +1,12 @@
+class FirstUnique:
+    def __init__(self, nums: List[int]):
+        self._queue = deque(nums)
+
+    def showFirstUnique(self):
+        for item in self._queue:
+            if self._queue.count(item) == 1:
+                return item
+        return -1
+
+    def add(self, value):
+        self._queue.append(value)
